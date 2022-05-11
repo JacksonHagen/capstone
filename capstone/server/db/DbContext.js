92548403b78f525498a7244356e05696f32acc5a
JacksonHagen <jackson.d.hagen@gmail.com>
2022-05-11 15:19:36 -0600

    Habit Schema

--- capstone/server/db/DbContext.js
+++ capstone/server/db/DbContext.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { HabitSchema } from '../models/Habit.js';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Habits = mongoose.model('Habit', HabitSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ capstone/server/models/Habit.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+export const HabitSchema = new Schema ({
+    title: {type: String, required: true},
+    inspo: {type: String, default: false},
+    accountId: {type: Schema.Types.ObjectId, required: true, ref: 'Account'},
+    streak: {type: Number, default: 0},
+    lastTracked: {type: Array},
+    isActive: {type: Boolean, default: true},
+    interval: {type: String, enum: ['day', 'week', 'month']},
+    maxStreak: {type: Number, default: 0}
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+  }
+);
+HabitSchema.virtual('creator', {
+  localField: 'accountId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
