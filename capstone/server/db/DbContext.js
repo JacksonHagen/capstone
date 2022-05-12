e7e09bb40eb285c58e166d6c87142ae601427b35
mykealw <mykeal.wheeler@gmail.com>
2022-05-12 11:07:06 -0600

    env in and adding to dbcontexts

--- capstone/server/db/DbContext.js
+++ capstone/server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { AwardSchema } from '../models/Award.js';
 import { HabitSchema } from '../models/Habit.js';
 import { ValueSchema } from '../models/Value'
 
@@ -8,6 +9,8 @@ class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Habits = mongoose.model('Habit', HabitSchema);
+  Awards = mongoose.model('Award', AwardSchema);
+  
 }
 
 export const dbContext = new DbContext()
