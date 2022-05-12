80cf2b10d0b8926bc3021b563521590b5b3323db
Maria Walker <mariawalker3232@gmail.com>
2022-05-12 14:23:01 -0600

    Client Chnages

--- capstone.client/src/pages/HomePage.vue
+++ capstone.client/src/pages/HomePage.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <!-- Insert Habits here -->
+        <!-- TODO ternary to change habit color -->
+        <div
+          class="
+            taskbar
+            bg-primary
+            d-flex
+            justify-content-between
+            align-items-center
+            m-2
+            p-2
+            rounded
+          "
+        >
+          <div class="h4">Quit Smoking</div>
+          <label class="format-container">One</label>
+          <input type="checkbox" @click="completeHabit" />
+          <span class="checkmark"></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,21 +33,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
+// .input {
+// }
+
+// .format-container input {
+//   position: absolute;
+//   opacity: 0;
+//   cursor: pointer;
+//   height: 0;
+//   width: 0;
+// }
+// .custom-checkbox {
+// }
 </style>
