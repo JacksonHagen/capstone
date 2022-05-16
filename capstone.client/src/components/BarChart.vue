ff1a07cb69f8c00be515c12162fee9f2377c58de
JacksonHagen <jackson.d.hagen@gmail.com>
2022-05-16 14:54:03 -0600

    awards display on habits page

--- capstone.client/src/components/BarChart.vue
+++ capstone.client/src/components/BarChart.vue
@@ -5,12 +5,12 @@
 
 <script>
 import { Bar } from "vue-chartjs";
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import { computed, onMounted } from '@vue/runtime-core';
 import { AppState } from '../AppState.js';
 import { habitsService } from '../services/HabitsService.js';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
 
 export default {
   components: { Bar },
