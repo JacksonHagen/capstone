b3162037051e0909b94c98a909aba8f8949dc56e
Dylan Cade <dacade.code@gmail.com>
2022-05-15 21:59:17 -0600

    basic habit form component

---
+++ capstone.client/src/components/HabitForm.vue
@@ -0,0 +1,93 @@
+<template>
+  <!-- we need an input which forks based on inputs. -->
+  <!-- first ask is, what's the name of your habit? -->
+  <!-- two: how often do you want to be reminded of this task? = inteerval, default to 1 -->
+  <!-- three: choose a color? -->
+  <!-- four: lastly, tell us your inspriation for this habit (skip this part) -->
+  <!-- FEATURE: pick a time? we push that particular time => new Date(time entered as a string) -->
+
+  <form @submit="newHabit">
+    <div class="mb-3">
+      <label for="habit-title" class="form-label">Give it a title:</label>
+      <input
+        type="text"
+        class="form-control"
+        id="habit-title"
+        placeholder=""
+        v-model="formData.title"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="habit-inspo" class="form-label"
+        >Why are you making this Habit?:</label
+      >
+      <input
+        type="text"
+        class="form-control"
+        id="habit-inspo"
+        placeholder=""
+        v-model="formData.inspo"
+      />
+      <small id="helpId" class="form-text text-muted"
+        >This is optional, but we might show it to you later.</small
+      >
+    </div>
+    <div class="mb-3">
+      <label for="habit-color" class="form-label"
+        >Give it a recognizable color</label
+      >
+      <input
+        type="color"
+        class="form-control"
+        id="habit-color"
+        v-model="formData.color"
+      />
+      <small id="helpId" class="form-text text-muted"
+        >If you want, leave it blank and we'll pick it.</small
+      >
+    </div>
+    <div class="mb-3">
+      <label for="habit-interval">Choose a car:</label>
+      <select name="habit-interval" id="habit-interval">
+        <option selected value="1">Daily</option>
+        <option value="2">Every other day</option>
+        <option value="7">Weekly</option>
+        <option value="28">Monthly</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+
+<script>
+import { computed, ref } from "@vue/reactivity"
+import { logger } from "../utils/Logger"
+import Pop from "../utils/Pop"
+import { habitsService } from "../services/HabitsService"
+import { AppState } from "../AppState"
+export default {
+  setup() {
+    const formData = ref({})
+    return {
+      formData,
+      account: computed(() => AppState.account),
+      day: computed(() => AppState.day),
+      async newHabit() {
+        try {
+          formData.value.interval = document.getElementById('habit-interval').value
+          formData.value.accountId = this.account.id
+          formData.value.trackHistory = [this.day]
+          await habitsService.newHabit(formData.value)
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
+        }
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
