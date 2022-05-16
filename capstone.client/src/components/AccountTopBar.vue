5e59e93d391e0e4c7c051d0d3fcd47f8af610878
JacksonHagen <jackson.d.hagen@gmail.com>
2022-05-16 10:46:37 -0600

    Account page no charts

---
+++ capstone.client/src/components/AccountTopBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-2 bg-primary rounded p-3">
+        <h5>Habits: {{ myHabits.length }}</h5>
+        <h5>Badges: {{ awards.length }}</h5>
+      </div>
+
+      <div class="col-9 bg-primary rounded offset-1 p-3">
+        <p>
+          Inspo: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          vitae aut harum esse dolorem amet? Soluta porro maxime nisi vitae
+          exercitationem ut corporis nobis natus?
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { computed } from 'vue'
+import { AppState } from '../AppState'
+
+export default {
+  setup() {
+    return {
+      // TODO change back to myHabits
+      myHabits: computed(() => AppState.habits),
+      awards: computed(() => AppState.myAwards),
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
--- capstone.client/src/pages/AccountPage.vue
+++ capstone.client/src/pages/AccountPage.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div>
+  <AccountTopBar />
+  <AccountCharts />
 </template>
 
 <script>
@@ -13,6 +10,7 @@ export default {
   name: 'Account',
   setup() {
     return {
+
       account: computed(() => AppState.account)
     }
   }
@@ -20,7 +18,4 @@ export default {
 </script>
 
 <style scoped>
-img {
-  max-width: 100px;
-}
 </style>
