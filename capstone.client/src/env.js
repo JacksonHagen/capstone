0ac9461153984b6bfd1e18a1fb219194c1339b4b
EricMGustafson <emgustafson22@gmail.com>
2022-05-19 15:22:36 -0600

    firebase prework

--- capstone.client/src/env.js
+++ capstone.client/src/env.js
@@ -4,3 +4,12 @@ export const useSockets = true
 export const domain = 'dev-wvrfwzhq.us.auth0.com'
 export const clientId = 'lAcTCVPJK7nDO2DAFlGznd09I1Z68b49'
 export const audience = 'https://JackDev.com'
+export const firebaseConfig = {
+  apiKey: "AIzaSyDeQ2_okhLsFQDNMaciif7MfHxosUmF33Y",
+  authDomain: "ontrak-capstone.firebaseapp.com",
+  projectId: "ontrak-capstone",
+  storageBucket: "ontrak-capstone.appspot.com",
+  messagingSenderId: "289407244703",
+  appId: "1:289407244703:web:c560af06ddfd19f9ecc5aa",
+  measurementId: "G-KBZEQ8SECW"
+};
